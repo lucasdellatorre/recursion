662730fd48e6fd12c6dc3e2d1395d950bccf1b83
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-10-01 23:25:13 -0300

    isListSorted

--- data-structures/src/recursion/Recursion.java
+++ data-structures/src/recursion/Recursion.java
@@ -1,5 +1,8 @@
 package recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Recursion {
     public static int peasantMultiply(int x, int y) {
         if (x == 0) return 0;
@@ -13,5 +16,18 @@ public class Recursion {
         }
         System.out.println("prod: " + prod);
         return prod;
+    }
+
+    public static int power(int base, int exp) {
+        if (exp == 0) return 1;
+        return base * power(base, exp - 1);
+    }
+
+    public static boolean isListSorted(List<Integer> arr) {
+        int n = arr.size();
+        if (n <= 1) {
+            return true;
+        }
+        return (isListSorted(arr.subList(0, n / 2))) && arr.get(n / 2 - 1) <= arr.get(n / 2) && isListSorted(arr.subList(n / 2, n));
     }
 }
